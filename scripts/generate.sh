c21a63cf7e6e055363d5cda11acfc508bdab8ab5
Jungtaek Kim <jt81.kim@samsung.com>
2020-03-26 14:15:52 +0900

    [fix] add build-base and alpine-sdk

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,7 +4,7 @@ echo "FROM docker:$DOCKER_VERSION"
 
 # install dependencies for aws cli
 echo "RUN apk update"
-echo "RUN apk add curl py-pip bash jq"
+echo "RUN apk add alpine-sdk build-base curl py-pip bash jq"
 echo "RUN pip install awscli==1.16.60"
 
 if [ ! -e $NODE_VERSION_NUM ] ; then
