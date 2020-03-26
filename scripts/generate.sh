4cf9364dc00c9d38517ae2c4dfb607d0a1c81540
Jungtaek Kim <jt81.kim@samsung.com>
2020-03-26 14:22:16 +0900

    [fix] only install packages for aws cli

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,27 +4,7 @@ echo "FROM docker:$DOCKER_VERSION"
 
 # install dependencies for aws cli
 echo "RUN apk update"
-echo "RUN apk add alpine-sdk build-base curl py-pip bash jq"
+echo "RUN apk add curl py-pip bash jq"
 echo "RUN pip install awscli==1.16.60"
 
-if [ ! -e $NODE_VERSION_NUM ] ; then
-    echo "RUN wget https://nodejs.org/dist/v$NODE_VERSION_NUM/node-v$NODE_VERSION_NUM.tar.gz && \
-    tar -xzvf node-v$NODE_VERSION_NUM.tar.gz && \
-    rm node-v$NODE_VERSION_NUM.tar.gz && \
-    cd node-v$NODE_VERSION_NUM && \
-    ./configure && \
-    make -j4 && \
-    make install && \
-    cd .. && \
-    rm -r node-v$NODE_VERSION_NUM"
-fi
-
-if [ ! -e $PYTHON_VERSION_NUM ] ; then
-    echo "RUN wget https://www.python.org/ftp/python/$PYTHON_VERSION_NUM/Python-$PYTHON_VERSION_NUM.tgz && \
-    tar xzf Python-$PYTHON_VERSION_NUM.tgz && \
-    rm Python-$PYTHON_VERSION_NUM.tgz && \
-    cd Python-$PYTHON_VERSION_NUM && \
-    ./configure && \
-    make install"
-fi
 
