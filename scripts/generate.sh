4530ee909bd77076d6f38d4f9aa5a2876278042b
Jungtaek Kim <jt81.kim@samsung.com>
2020-03-26 13:49:54 +0900

    [fix] remove apt-get

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,7 +3,7 @@
 echo "FROM docker:$DOCKER_VERSION"
 
 # install dependencies for aws cli
-echo "RUN apt-get update"
+echo "RUN apk update"
 echo "RUN apk add curl py-pip bash jq"
 echo "RUN pip install awscli==1.16.60"
 
