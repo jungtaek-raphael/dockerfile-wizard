5f3de6f6880640d76359c76dd99ee1cebd50f0ff
jungtaek.kim <jt81.kim@samsung.com>
2020-05-11 12:30:17 +0900

    [feat] Add libffi-dev package
    Signed-off-by: jungtaek.kim <jt81.kim@samsung.com>

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,4 +4,4 @@ echo "FROM python:3.6-alpine"
 
 # install dependencies
 echo "RUN apk update"
-echo "RUN apk --no-cache add gcc build-base linux-headers postgresql-dev"
+echo "RUN apk --no-cache add gcc build-base linux-headers postgresql-dev libffi-dev"
