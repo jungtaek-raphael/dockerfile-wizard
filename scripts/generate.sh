73bd9b0935f46182fbc496f7597feaaf1749d14d
Jungtaek Kim <jt81.kim@samsung.com>
2020-08-11 18:19:18 +0900

    Install virtualenv
    Signed-off-by: Jungtaek Kim <jt81.kim@samsung.com>

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,4 +5,4 @@ echo "FROM docker:$DOCKER_VERSION"
 # install dependencies for aws cli
 echo "RUN apk update"
 echo "RUN apk add --no-cache curl git openssh python3 py-pip bash jq"
-echo "RUN pip install awscli==1.16.60"
+echo "RUN pip install awscli==1.16.60 virtualenv"
