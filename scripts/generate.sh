9dd4fc5294023e19967eadfd75ec4a5094bba9d9
Jungtaek Kim <jt81.kim@samsung.com>
2020-08-11 16:24:42 +0900

    [fix] Pre-install python dependencies
    To reduce build time, pre-install some packages.
    Especially pandas, numpy, cryptography installation takes long time because those packages are not built for alpine.
    It means that those packages should be built and installed base source code during every docker build.
    This is not efficient so we need to pre-install those packages.
    
    Signed-off-by: Jungtaek Kim <jt81.kim@samsung.com>

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,4 +4,6 @@ echo "FROM python:3.6-alpine"
 
 # install dependencies
 echo "RUN apk update"
-echo "RUN apk --no-cache add gcc build-base linux-headers postgresql-dev libffi-dev"
+echo "RUN apk add --no-cache curl git openssh py-pip bash jq gcc build-base python3-dev linux-headers musl-dev postgresql-dev libffi-dev"
+echo "RUN python3 -m pip install --upgrade pip"
+echo "RUN pip install awscli==1.16.60 pylint==2.4.4 PyYAML==5.3 pytz==2019.3 cryptography==2.9.2 pandas==1.0.4 numpy==1.19.1"
