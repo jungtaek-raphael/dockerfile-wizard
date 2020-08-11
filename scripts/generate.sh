908cd4164789c8eeb2bbf54149b1d3dad02fbcca
Jungtaek Kim <jt81.kim@samsung.com>
2020-08-11 16:24:42 +0900

    Pre-install python dependencies
    Signed-off-by: Jungtaek Kim <jt81.kim@samsung.com>

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -4,5 +4,6 @@ echo "FROM docker:$DOCKER_VERSION"
 
 # install dependencies for aws cli
 echo "RUN apk update"
-echo "RUN apk add --no-cache curl git openssh py-pip bash jq"
-echo "RUN pip install awscli==1.16.60"
+echo "RUN apk add --no-cache curl git openssh py-pip bash jq gcc python3-dev linux-headers musl-dev libffi-dev"
+echo "RUN python3 -m pip install --upgrade pip"
+echo "RUN pip install awscli==1.16.60 pylint==2.4.4 PyYAML==5.3 pytz==2019.3 cryptography==2.9.2 pandas==1.0.4 numpy==1.19.1"
